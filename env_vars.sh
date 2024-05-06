a6959bcbc687e33949f077d7a676abb60ed8442b
EC2 Default User <ec2-user@ip-172-31-8-212.us-west-1.compute.internal>
2024-05-06 18:43:04 +0000

    fix some pip bugs

---
+++ env_vars.sh
@@ -0,0 +1,4 @@
+export CPLUS_INCLUDE_PATH=/mnt/data/cs348k/gaussian-splatting/submodules/diff-gaussian-rasterization/third_party/glm:$CPLUS_INCLUDE_PATH
+export C_INCLUDE_PATH=/mnt/data/cs348k/gaussian-splatting/submodules/diff-gaussian-rasterization/third_party/glm:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/mnt/data/cs348k/gaussian-splatting/submodules/diff-gaussian-rasterization/third_party/glm:$CPLUS_INCLUDE_PATH
+export C_INCLUDE_PATH=/mnt/data/cs348k/gaussian-splatting/submodules/diff-gaussian-rasterization/third_party/glm:$C_INCLUDE_PATH
